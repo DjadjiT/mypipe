6c74733c0b039d579d5d40662fcfcc91bf721f04
DjadjiT <djadji.traore75@gmail.com>
2019-12-19 00:43:30 +0100

    Changement de pc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate clap;
+use clap::{Arg,App, SubCommand};
+
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("My Pipe")
+       .version("1.0")
+       .about("Does great things & some pipe")
+       .author("Djadji Traore")
+       .args(
+         Arg::with_name("in")
+          .long("in")
+          .takes_value(true)
+          .help("In command")
+          .required(true);
+     )
+     .args(
+         Arg::with_name("out")
+            .long("out")
+            .takes_value(true)
+            .help("out command")
+            .required(true)
+     )
+    .get_matches();
 }
